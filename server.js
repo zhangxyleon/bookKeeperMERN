b797289522c1c92923b91b2b8cd0fdc15fccc1df
Xiangyu Zhang <leonfigocr17@gmail.com>
2020-09-29 00:44:27 -0400

    heroku

--- server.js
+++ server.js
@@ -31,7 +31,16 @@ app.use(passport.initialize());
 app.use(express.json());
 app.use('/users', require('./routes/userRoute'));
 app.use('/collections', require('./routes/bookmarkRoute.js'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
 
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+const PORT = process.env.PORT || 5000;
 app.listen(app.get('port'), (server) => {
 	console.info(`Server listen on port ${app.get('port')}`);
 });
